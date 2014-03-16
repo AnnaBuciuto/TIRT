1f256157620d25c619843a3d56a5b1fd8a048ee0
Kamil Chłodnicki <kchlodnicki@imagin.pl>
2014-03-16 19:11:14 +0100

    add map controller

--- tirt/src/main/Main.java
+++ tirt/src/main/Main.java
@@ -1,14 +1,23 @@
 package main;
 
+import algorithm.RandomPlacingStrategy;
 import input.MapReader;
+import map.NetworkMap;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        String fileName = "tirt/maps/map1.txt";
+        String fileName = "tirt/maps/map2.txt";
         MapReader reader = new MapReader();
-        reader.readMap(fileName);
+        NetworkMap map = reader.readMapFromFile(fileName);
+
+        MapController controller = new MapController();
+        controller.setMap(map);
+        controller.setStrategy(new RandomPlacingStrategy());
+        controller.placeAccessPoints();
+
+        System.out.println(map);
     }
 
 }
---
+++ tirt/src/main/MapController.java
@@ -0,0 +1,23 @@
+package main;
+
+import algorithm.PlacingStrategy;
+import map.NetworkMap;
+
+public class MapController {
+
+    private PlacingStrategy strategy;
+    private NetworkMap map;
+
+    public void setMap(NetworkMap map) {
+        this.map = map;
+    }
+
+    public void setStrategy(PlacingStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void placeAccessPoints() {
+        strategy.placeAccessPoints();
+    }
+
+}
